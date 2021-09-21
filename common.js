029cb9477d4ede3419469d27d9b514b606053613
moomfakh <89586272+moomfakh@users.noreply.github.com>
2021-09-21 16:43:25 +0530

    Update common.js

--- common.js
+++ common.js
@@ -1,3 +1,7 @@
 function nav(uri) {
 window.location.href = uri; 
 }
+function forward(current , next) {
+document.getElementsByClassName("con")[next].style.display = "block"  ;
+document.getElementsByClassName("con")[current].style.display = "none"  ;
+}
