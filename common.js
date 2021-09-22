7e960a7da5cbbc597da4a8adf1e34fd229f76ba3
moomfakh <89586272+moomfakh@users.noreply.github.com>
2021-09-22 09:19:37 +0530

    Update common.js

--- common.js
+++ common.js
@@ -5,3 +5,6 @@ function forward(current , next) {
 document.getElementsByClassName("con")[next].style.display = "block"  ;
 document.getElementsByClassName("con")[current].style.display = "none"  ;
 }
+function setlevel(num , unit) {
+localStorage.setItem(unit , num)
+}
